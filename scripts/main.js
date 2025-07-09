3791042997826698226ec6217349825859bd62d9
JoseVelez9130 <tecnologyclass2005@gmail.com>
2025-07-09 13:38:12 -0500

    Segundo Commit: Modulo de login

--- scripts/main.js
+++ scripts/main.js
@@ -9,9 +9,12 @@ document.getElementById('newsletterForm').addEventListener('submit', function(e)
 });
 
 // 2. Manejo del boton de Login
-document.getElementById('loginBtn').addEventListener('click', () => {
-  alert('Redirigiendo a Iniciar Sesión'); // Simula redirección mostrando alerta
-});
+// document.getElementById('loginBtn').addEventListener('click', () => {
+//   alert('Redirigiendo a Iniciar Sesión'); // Simula redirección mostrando alerta
+// });
+document.getElementById('loginBtn').addEventListener('click', function () {
+    window.location.href = 'login.html'});
+
 
 // 3. Manejo del botón de Registro
 document.getElementById('registerBtn').addEventListener('click', () => {
@@ -23,8 +26,5 @@ NOTAS:
 - El código JavaScript maneja la interactividad de la página, como el envío del formulario de newsletter y los botones de autenticación.
 - Se utiliza `addEventListener` para escuchar eventos de envío del formulario y clics en los botones.
 - El navegador trae una validacion nativa para el campo de email, por lo que no es necesario agregar validaciones adicionales, 
-porque el campo esta definido como type="email" lo que activa validaciones automáticas del navegador.
-    
-
-
+porque el campo esta definido como type="email" lo que activa validaciones automáticas del navegador.    
 */
